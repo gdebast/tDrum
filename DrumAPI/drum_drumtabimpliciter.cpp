0e5b4c216a464527daad6586618c1ec63d378d0e
gdebast <gatien.debast@gmail.com>
2020-05-22 10:28:27 +0200

    implicit/explicit printing : create explicit method

--- DrumAPI/drum_drumtabimpliciter.cpp
+++ DrumAPI/drum_drumtabimpliciter.cpp
@@ -13,18 +13,16 @@ std::vector<std::pair<std::vector<Drum::DrumTabPart*> , unsigned> >
 DrumTabImpliciter::getImplicit(const std::vector<std::pair<Drum::DrumTabPart *, bool> > &drumRabParts_bool) const
 {
     std::vector<std::pair<std::vector<Drum::DrumTabPart*> , unsigned> >  returnedImplicitTab;
-    std::vector<Drum::DrumTabPart*> currenttabtoFollow{};
+    std::vector<Drum::DrumTabPart*> currentTabToFollow{};
 
     for (auto it =  drumRabParts_bool.cbegin(); it != drumRabParts_bool.cend(); it++)
     {
-
-
 
         // the drum tab part is explicit so we simply copy it
         // the first drum tab part is obviously explicit
         if (it->second == false || it == drumRabParts_bool.cbegin())
         {
-            currenttabtoFollow.push_back(it->first);
+            currentTabToFollow.push_back(it->first);
             continue;
         }
 
@@ -32,4 +30,17 @@ DrumTabImpliciter::getImplicit(const std::vector<std::pair<Drum::DrumTabPart *, 
         // we have to test that the previous part are identical
 
     }
+}
+
+std::vector<std::pair<std::vector<DrumTabPart *>, unsigned> >
+DrumTabImpliciter::getExplicit(const std::vector<std::pair<DrumTabPart *, bool> > &drumRabParts_bool) const
+{
+    std::vector<Drum::DrumTabPart*> currentTabToFollow{};
+    for (const auto& pair : drumRabParts_bool)
+    {
+        auto* drumTabPart = pair.first;
+        currentTabToFollow.push_back(drumTabPart);
+    }
+
+    return std::vector<std::pair<std::vector<DrumTabPart *>, unsigned> >{std::make_pair(currentTabToFollow,1)};
 }
--- DrumAPI/drum_drumtabimpliciter.h
+++ DrumAPI/drum_drumtabimpliciter.h
@@ -12,6 +12,7 @@ namespace Drum
     {
     public:
         DrumTabImpliciter();
+
         /*
          * this method return a vector of vector of DrumTabPart followed by
          * the number of times this vector of DrumTabPart is repeated.
@@ -20,6 +21,16 @@ namespace Drum
          */
         std::vector<std::pair<std::vector<Drum::DrumTabPart*> , unsigned> >
         getImplicit(const std::vector<std::pair<DrumTabPart*,bool>>& drumRabParts_bool) const;
+
+        /*
+         * this method return a vector of vector of DrumTabPart followed by
+         * the number of times this vector of DrumTabPart is repeated.
+         * This is explicit version so in input vector will be copied in the first pair
+         * with the number 1, meaning the entire vector is present once.
+         */
+        std::vector<std::pair<std::vector<Drum::DrumTabPart *>, unsigned> >
+        getExplicit(const std::vector<std::pair<DrumTabPart*,bool>>& drumRabParts_bool) const;
+
 
     };
 }
